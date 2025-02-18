5f0757728ef0a55e9936a3d819f72df6a27d8e64
Daniel <daniel.jorge96@outlook.com>
2025-02-18 15:08:32 -0300

    Refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { useEffect, useMemo, useState, type JSX } from 'react'
 import Select from 'react-select'
 import useGetCareerNames from './hooks/useGetCareerNames'
-import { CareerHeader } from './components/CareerHeader'
+// import { CareerHeader } from './components/CareerHeader'
 import { getCareer } from './api/careerApi'
 import { type Career } from './types/types'
 import { CareerDetails } from './components/CareerDetails'
+import { Header } from './components/Header'
 
 function App(): JSX.Element {
   const { careerNames: careerNamesApi, careerNamesError } = useGetCareerNames()
 
   const [careerSelected, setCareerSelected] = useState<string | null>(null)
-  // const [careerNames, setCareerNames] = useState<
-  //   { label: string; value: string }[]
-  // >([])
   const [career, setCareer] = useState<Career | null>(null)
   const [error, setError] = useState<string | null>(null)
 
@@ -36,23 +34,10 @@ function App(): JSX.Element {
       .map((el) => ({ label: el.name, value: el._id }))
   }, [careerNamesApi])
 
-  // useEffect(() => {
-  //   if (!careerNamesApi.length) return
-
-  //   const careerNames: { label: string; value: string }[] = []
-
-  //   careerNamesApi.forEach((el) => {
-  //     if (el && el.name && el._id)
-  //       careerNames.push({ label: el.name, value: el._id })
-  //   })
-
-  //   setCareerNames(careerNames)
-  // }, [careerNamesApi])
-
   useEffect(() => {
-    const initilizeCareer = async (): Promise<void> => {
-      if (!careerSelected) return
+    if (!careerSelected) return
 
+    const initilizeCareer = async (): Promise<void> => {
       try {
         const careerData = await getCareer(careerSelected)
         setCareer(careerData)
@@ -79,29 +64,7 @@ function App(): JSX.Element {
 
   return (
     <>
-      <header className="bg-fourth-color shadow-shadow-select px-4 py-2 lg:px-0">
-        <div className="mx-auto flex max-w-4xl flex-col items-start gap-4 px-2 sm:flex-row sm:items-center sm:gap-16 md:min-w-4xl lg:justify-between">
-          <div className="order-0 mx-auto sm:mx-0">
-            <a
-              href="https://www.uno.edu.ar/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <img
-                className="hidden w-28 sm:block sm:w-24"
-                src="../src/assets/logoUNO.svg"
-                alt="Logo de la Universidad Nacional del Oeste"
-              />
-              <span className="inline-block border-y-2 text-center text-3xl font-extrabold tracking-wide text-white sm:hidden">
-                UNO
-              </span>
-            </a>
-          </div>
-          {careerHeaderInfo && (
-            <CareerHeader careerHeaderInfo={careerHeaderInfo} />
-          )}
-        </div>
-      </header>
+      <Header careerHeaderInfo={careerHeaderInfo} />
       <main>
         <div className="w-full">
           <Select
---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { type CareerHeaderInfo } from '../types/types'
+import { CareerHeader } from './CareerHeader'
+
+interface HeaderProps {
+  careerHeaderInfo?: CareerHeaderInfo | null
+}
+
+export const Header: React.FC<HeaderProps> = ({ careerHeaderInfo }) => {
+  return (
+    <header className="bg-fourth-color shadow-shadow-select px-4 py-2 lg:px-0">
+      <div className="mx-auto flex max-w-4xl flex-col items-start gap-4 px-2 sm:flex-row sm:items-center sm:gap-16 md:min-w-4xl lg:justify-between">
+        <div className="order-0 mx-auto sm:mx-0">
+          <a
+            href="https://www.uno.edu.ar/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <img
+              className="hidden w-28 sm:block sm:w-24"
+              src="../src/assets/logoUNO.svg"
+              alt="Logo de la Universidad Nacional del Oeste"
+            />
+            <span className="inline-block border-y-2 text-center text-3xl font-extrabold tracking-wide text-white sm:hidden">
+              UNO
+            </span>
+          </a>
+        </div>
+        {careerHeaderInfo && (
+          <CareerHeader careerHeaderInfo={careerHeaderInfo} />
+        )}
+      </div>
+    </header>
+  )
+}
