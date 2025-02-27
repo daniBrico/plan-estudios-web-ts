03fe86b6cb3ae416af5931009ac319dec30336c0
Daniel <daniel.jorge96@outlook.com>
2025-02-27 20:56:26 -0300

    Refactoring code

--- src/components/CareerDetails.tsx
+++ src/components/CareerDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { JSX } from 'react'
 import { type SubjectsByYear, type Subject } from '../types/types'
 import { TableRows } from './TableRow'
 
@@ -11,29 +11,40 @@ const ListOfTables: React.FC<ListOfTablesProps> = ({
   tableTitle,
   subjects
 }) => {
+  const titles = [
+    { label: 'code', value: 'Código', classes: 'rounded-tl-lg' },
+    { label: 'subject', value: 'Materia', classes: 'text-left' },
+    { label: 'offering', value: 'Dictado', classes: '' },
+    { label: 'correlatives', value: 'Correlativas', classes: 'w-32' },
+    { label: 'state', value: 'Estado', classes: 'rounded-tr-lg w-32' }
+  ]
+
+  const renderTableHeads = (
+    label: string,
+    value: string,
+    clasess: string
+  ): JSX.Element => {
+    return (
+      <th
+        key={label}
+        className={`border-back border-b-2 p-2 text-base font-bold tracking-wide ${clasess}`}
+      >
+        {value}
+      </th>
+    )
+  }
+
   return (
     <>
-      <h2 className="bg-first-color md:text-first-color sticky top-2 z-20 mt-4 mb-2 rounded-tl-md rounded-tr-md py-1 text-center text-xl text-white md:static md:rounded-none md:bg-white md:text-2xl">
+      <h2 className="bg-first-color md:text-first-color sticky top-2 z-20 mt-4 mb-2 rounded-tl-md rounded-tr-md py-1 text-center text-xl text-white md:static md:rounded-none md:bg-inherit md:text-2xl">
         {tableTitle}
       </h2>
       <table className="md:shadow-shadow-box mx-auto mb-5 w-full rounded-t-lg rounded-tl-lg rounded-b-lg">
         <thead className="bg-first-color top-2 z-20 hidden rounded-t-lg md:sticky md:table-header-group">
           <tr className="rounded-t-lg text-white">
-            <th className="border-back rounded-tl-lg border-b-2 p-2 text-base font-bold tracking-wide">
-              Código
-            </th>
-            <th className="border-back border-b-2 p-2 text-left text-base font-bold tracking-wide">
-              Materia
-            </th>
-            <th className="border-back border-b-2 p-2 text-base font-bold tracking-wide">
-              Dictado
-            </th>
-            <th className="border-back w-32 border-b-2 p-2 text-base font-bold tracking-wide">
-              Correlativas
-            </th>
-            <th className="border-back w-32 rounded-tr-lg border-b-2 p-2 text-base font-bold tracking-wide">
-              Estado
-            </th>
+            {titles.map((title) =>
+              renderTableHeads(title.label, title.value, title.classes)
+            )}
           </tr>
         </thead>
         <tbody className="grid grid-cols-1 gap-1 text-black sm:grid-cols-2 md:table-row-group">
