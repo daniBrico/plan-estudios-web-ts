c6f3fa6b83af76049f0ffa4414d7840726057593
Daniel <daniel.jorge96@outlook.com>
2025-03-11 17:33:24 -0300

    Added diferents sizes for spinner loading

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const LoadingSpinner: React.FC = () => {
   return (
     <>
-      <div className="animate-loading-spinner border-t-first-color mx-auto box-border h-11 w-11 rounded-full border-[10px] border-solid border-transparent" />
+      <div className="animate-loading-spinner border-t-first-color mx-auto box-border h-11 w-11 rounded-full border-[10px] border-solid border-transparent md:h-14 md:w-14 xl:h-16 xl:w-16" />
     </>
   )
 }
