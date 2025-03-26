d921f1abe2eb59b195e97340da82a4b3ee5ef6ee
Daniel <daniel.jorge96@outlook.com>
2025-03-26 16:09:18 -0300

    Resolving some visual bugs

--- src/components/Correlative.tsx
+++ src/components/Correlative.tsx
@@ -5,7 +5,7 @@ import { useCareerContext } from '../hooks/useCareerContext'
 
 const ToolTip = (name: Name | undefined): JSX.Element => {
   return (
-    <span className="bg-first-color invisible absolute bottom-full left-1/2 z-50 -translate-x-1/2 transform rounded-sm border-2 border-white px-1.5 py-1 text-center whitespace-nowrap text-white group-hover:visible">
+    <span className="bg-first-color invisible absolute bottom-full left-1/2 z-[130] -translate-x-1/2 transform rounded-sm border-2 border-white px-1.5 py-1 text-center whitespace-nowrap text-white group-hover:visible">
       {name?.shortName || name?.longName}
     </span>
   )
@@ -30,9 +30,7 @@ export const Correlative: React.FC<CorrelativeProps> = ({
 
   return (
     <>
-      <div
-        className={`group relative z-[105] inline-block text-left ${cssClasess}`}
-      >
+      <div className={`group relative inline-block text-left ${cssClasess}`}>
         <span className={`${setClassForState(actualState)} cursor-pointer`}>
           {correlative}
         </span>
