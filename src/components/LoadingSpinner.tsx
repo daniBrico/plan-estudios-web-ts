f8ddb1960cabd2bc17ada9a9781641e567ab7862
Daniel <daniel.jorge96@outlook.com>
2025-04-03 22:46:03 -0300

    Solving text-size from description spinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -9,7 +9,9 @@ export const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ message }) => {
     <>
       <div className="absolute flex w-full flex-col items-center justify-center gap-2">
         <div className="animate-loading-spinner border-t-first-color mx-auto box-border h-11 w-11 rounded-full border-[10px] border-solid border-transparent md:h-14 md:w-14 xl:h-16 xl:w-16" />
-        {message && <p className="text-first-color">{message}</p>}
+        {message && (
+          <p className="text-first-color text-sm md:text-base">{message}</p>
+        )}
       </div>
     </>
   )
