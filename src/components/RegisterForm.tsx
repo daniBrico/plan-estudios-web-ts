0453f41c2ab75db6e2858d6b8416797a9e0b1d8b
Daniel <daniel.jorge96@outlook.com>
2025-11-21 00:37:23 -0300

    Validations with zod

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,19 +1,31 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { type JSX } from 'react'
 import { type SubmitHandler, useForm } from 'react-hook-form'
+import { z } from 'zod'
 
-type FormFields = {
-  name: string
-  lastName: string
-  email: string
-  password: string
-}
+const userSchema = z.object({
+  name: z
+    .string()
+    .min(2, 'El nombre debe tener al menos dos caracteres')
+    .max(50, 'El nombre no debe superar los 50 caracteres'),
+  lastName: z
+    .string()
+    .min(2, 'El apellido debe tener al menos dos caracteres')
+    .max(50, 'El apellido no debe superar los 50 caracteres'),
+  email: z.email('Debe ser un email valido'),
+  password: z.string().min(8, 'La contraseña debe tener al menos 8 caracteres')
+})
+
+type FormFields = z.infer<typeof userSchema>
 
 const RegisterForm = (): JSX.Element => {
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting }
-  } = useForm<FormFields>()
+  } = useForm<FormFields>({
+    resolver: zodResolver(userSchema)
+  })
 
   const onSubmit: SubmitHandler<FormFields> = async (data) => {
     await new Promise((resolve) => setTimeout(resolve, 1000))
@@ -29,39 +41,24 @@ const RegisterForm = (): JSX.Element => {
       <h1 className="text-lg font-semibold text-gray-700 md:text-2xl lg:text-3xl dark:text-stone-200">
         Ingrese sus datos
       </h1>
-      <input
-        {...register('name', {
-          required: 'El nombre es requerido'
-        })}
-        type="text"
-        placeholder="Nombre"
-      />
+      <input {...register('name')} type="text" placeholder="Nombre" />
       {errors.name && <div className="text-red-500">{errors.name.message}</div>}
-      <input
-        {...register('lastName', {
-          required: 'El apellido es requerido'
-        })}
-        type="text"
-        placeholder="Apellido"
-      />
-      <input
-        {...register('email', {
-          required: 'El mail es requerido'
-        })}
-        type="text"
-        placeholder="Mail"
-      />
+      <input {...register('lastName')} type="text" placeholder="Apellido" />
+      {errors.lastName && (
+        <div className="text-red-500">{errors.lastName.message}</div>
+      )}
+      <input {...register('email')} type="text" placeholder="Mail" />
       {errors.email && (
         <div className="text-red-500">{errors.email.message}</div>
       )}
       <input
-        {...register('password', {
-          required: 'La contraseña es requerida',
-          minLength: 8
-        })}
+        {...register('password')}
         type="password"
         placeholder="Contraseña"
       />
+      {errors.password && (
+        <div className="text-red-500">{errors.password.message}</div>
+      )}
       <div className="flex w-full justify-center">
         <button
           className="cursor-pointer rounded-md bg-gray-600 px-2 py-1 text-lg text-white transition-all duration-300 ease-in-out hover:bg-gray-500 dark:bg-stone-700 dark:hover:bg-stone-800"
