f2123daeb7d3a85500dfb2b99af4632c9fff79b7
Daniel <daniel.jorge96@outlook.com>
2025-11-29 16:53:28 -0300

    Enable withCredentials to send cookies in requests

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -1,17 +1,8 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL
-  // withCredentials: true // si usas cookies JWT
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true
 })
-
-/* httpClient.interceptors.request.use((config) => {
-  // Opcional: agregar token manualmente si lo guardás en localStorage
-  // const token = localStorage.getItem("token");
-  // if (token) config.headers.Authorization = `Bearer ${token}`;
-
-  return config
-})
-*/
 
 export default httpClient
