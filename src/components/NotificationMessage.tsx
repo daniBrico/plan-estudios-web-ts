cbe85680d54b96fd009f8b3b724f08b68b607ea0
Daniel <daniel.jorge96@outlook.com>
2025-12-20 00:53:43 -0300

    Add styles for dark and responsive mode

--- src/components/NotificationMessage.tsx
+++ src/components/NotificationMessage.tsx
@@ -12,14 +12,16 @@ const NotificationMessage: React.FC<NotificationMessageProps> = ({
   return (
     <div
       className={classNames(
-        'pointer-events-none absolute top-full z-50 mt-1 flex w-full items-center justify-center rounded-md border border-gray-200 bg-white py-2 shadow-md transition-all duration-300 ease-in-out',
+        'pointer-events-none absolute top-full z-50 mt-2 flex w-full items-center justify-center rounded-md border border-gray-200 bg-white px-2 py-2 shadow-md transition-all duration-300 ease-in-out dark:border-stone-800 dark:bg-stone-900',
         {
           'translate-y-0 opacity-100': show,
           '-translate-y-full opacity-0': !show
         }
       )}
     >
-      <p className="text-center text-sm text-pretty md:text-base">{message}</p>
+      <p className="text-center text-sm text-pretty text-gray-800 md:text-base dark:text-stone-300">
+        {message}
+      </p>
     </div>
   )
 }
