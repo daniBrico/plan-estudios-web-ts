e6e5eafe44913605f284eabb33c0a0fcec3d7fc0
Daniel <daniel.jorge96@outlook.com>
2026-01-28 01:53:53 -0300

    Change range limit in passwordStrong

--- src/schemas/fields/passwordStrong.schema.ts
+++ src/schemas/fields/passwordStrong.schema.ts
@@ -1,6 +1,8 @@
 import { passwordSchema } from './password.schema'
 
 export const passwordStrongSchema = passwordSchema
+  .min(8, 'Mínimo 8 caracteres')
+  .max(24, 'Máximo 24 caracteres')
   .regex(/[a-z]/, 'Debe contener al menos una minúscula')
   .regex(/[A-Z]/, 'Debe contener al menos una mayúscula')
   .regex(/[0-9]/, 'Debe contener al menos un número')
